60ace22c66f89dd654f437c796b59ce68eeae87c
Tausif <tsftousif2@gmail.com>
2023-04-30 18:39:07 +0800

    All things are ready to (Before Sidebar)

---
+++ src/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./css/Sidebar.css"
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <h1>
+        Side bar
+      </h1>
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDw1mSZBCBBbV392eyLiH8hDareZzwOE4",
+    authDomain: "whatsapp-clone-b850d.firebaseapp.com",
+    projectId: "whatsapp-clone-b850d",
+    storageBucket: "whatsapp-clone-b850d.appspot.com",
+    messagingSenderId: "286599850270",
+    appId: "1:286599850270:web:1b3085a45d92dcb4a9c10f",
+    measurementId: "G-LXE37HTV0D"
+  };
