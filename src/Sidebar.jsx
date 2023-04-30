40213458d22a03792c417330850dcb11fb1b6341
Tausif <tsftousif2@gmail.com>
2023-04-30 19:28:13 +0800

    Done styling header

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import "./css/Sidebar.css"
+import React from "react";
+import "./css/Sidebar.css";
+import { Avatar, IconButton } from "@mui/material";
+import { MoreVert, DonutLarge, Chat } from "@mui/icons-material";
 function Sidebar() {
   return (
-    <div className='sidebar'>
-      <h1>
-        Side bar
-      </h1>
-
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
